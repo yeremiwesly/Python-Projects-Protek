f3164e7b7590e9ba59664f49effed12bc6f9cc72
Yeremi Wesly Sinaga <72002433+yeremiwesly@users.noreply.github.com>
2020-10-18 22:30:58 +0700

    Add files via upload

---
+++ praktikum4(latihan2).py
@@ -0,0 +1,4 @@
+# program hitung banyak bensin yang diperlukan
+jarak = int(input("jarak tempuh = "))
+liter = jarak/12
+print("bensin yang diperlukan", liter ,"liter")
---
+++ praktikum4(latihan4).py
@@ -0,0 +1,11 @@
+print("Pak Amir menempuh waktu dari kota A ke C adalah?")
+waktuAwal = float(06.00)
+waktuJeda = float(0.45)
+jarakAB = int(125)
+jarakBC = int(256)
+kecepatan1 = int(62)
+kecepatan2 = int(70)
+waktuAB = round(jarakAB/kecepatan1)
+waktuBC = round(jarakBC/kecepatan2)
+waktusampai = waktuAwal + waktuAB + waktuBC + waktuJeda
+print("waktu sampai di kota C => pukul", waktusampai)
